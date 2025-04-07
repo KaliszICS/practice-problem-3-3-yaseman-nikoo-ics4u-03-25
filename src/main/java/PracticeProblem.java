9faaf9d95f37a728ef70cfed54aaf7b074cd1e99
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-04-07 17:56:23 +0000

    complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		
+		System.out.println(permsUnique("rro"));
 
 	}
 	public static ArrayList<String> perms(String word) {
 
 		ArrayList<String> perms = new ArrayList<String>();
 		permutationsHelper("", word, perms);
+		if (word.equals("")){
+			perms.add("");
+			return perms;
+		}
 		return perms;
 	}
 
@@ -28,26 +32,26 @@ public class PracticeProblem {
 	public static ArrayList<String> permsUnique(String word) {
 
 		ArrayList<String> perms = new ArrayList<String>();
-		permutationsHelper("", word, perms);
+		
+		permsUniqueHelper("", word, perms);
 		return perms;
 	}
 
-	public static void permsUnitqueHelper(String usedLetters, String unusedLetters, ArrayList<String> perms) {
+	public static void permsUniqueHelper(String usedLetters, String unusedLetters, ArrayList<String> perms) {
 		for(int i=0;i<unusedLetters.length();i++){
 			String newused =usedLetters + unusedLetters.substring(i, i+1);
 			String newunused = unusedLetters.substring(0, i)+ unusedLetters.substring(i+1, unusedLetters.length());
 		
 			if (newunused.length()== 0){
-				for(int j =0; j<perms.size();j++){
-					if (!newused.equals(perms.get(i))){
-						perms.add(newused);
-					}
-					
+				System.out.println("Here");
+				if (!perms.contains(newused)){
+					System.out.println("Here");
+					perms.add(newused);
 				}
 				
 			}
 			else{
-				permutationsHelper(newused, newunused, perms);
+				permsUniqueHelper(newused, newunused, perms);
 			}
 		}
 	}
